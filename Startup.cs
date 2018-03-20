3df0ab4376bc7757d77af2b69689bd579a75ea93
ravipark021 <ravi.park021@gmail.com>
2018-03-21 02:38:07 +0530

    some changes

--- Startup.cs
+++ Startup.cs
@@ -108,10 +108,11 @@ namespace CountriesListApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+            app.UseDeveloperExceptionPage();
 
             app.UseAuthentication();
 
